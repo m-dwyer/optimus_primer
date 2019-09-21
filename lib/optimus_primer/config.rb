c66eb92a140d7275d1537029bd7c94a5f2090ec2
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2019-09-21 10:07:29 +1000

    Refactor config

--- lib/optimus_primer/config.rb
+++ lib/optimus_primer/config.rb
@@ -3,9 +3,45 @@ require 'yaml'
 module OptimusPrimer
   class Config
     class << self
-      def load(file)
-        YAML.load(File.read(file))
+      def load_file(file)
+        load(YAML.load(File.read(file)))
       end
+
+      def load(config)
+        options = Options.new
+        options.source = config
+        options.load_options
+      end
+    end
+  end
+
+  class Options < OpenStruct
+    attr_accessor :source
+
+    def load_options
+      constructed_options = recursive_convert(@source)
+      marshal_load(constructed_options.marshal_dump)
+      self
+    end
+
+    private
+
+    def recursive_convert(hash)
+      s = self.class.new
+
+      hash.each do |k, v|
+        s.new_ostruct_member(k)
+
+        case v
+        when Hash
+          v = recursive_convert(v)
+        when Array
+          v = v.map { |e| e.is_a?(Hash) ? recursive_convert(e) : e }
+        end
+        s.send("#{k}=".to_sym, v)
+      end
+
+      s
     end
   end
 end
